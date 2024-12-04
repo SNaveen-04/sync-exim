d679aa2977472ec33819304562c9f0cca31e6fd1
Naveen <naveen80567@gmail.com>
2024-12-04 22:59:54 +0530

    Deploy init

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,19 +9,19 @@ const NavBar = () => {
         </div>
         <ul className="navigation">
             <li>
-                <Link className="link" to="/sync-exim">Home</Link>
+                <Link to="/sync-exim">Home</Link>
             </li>
             <li>
-                <Link className="link" to="/sync-exim/about">About</Link>
+                <Link to="/sync-exim/about">About</Link>
             </li>
             <li>
-                <Link className="link" to="/sync-exim/contact">Contact</Link>
+                <Link to="/sync-exim/contact">Contact</Link>
             </li>
             <li>
-                <Link className="link" to="/sync-exim/products">Products</Link>
+                <Link to="/sync-exim/products">Products</Link>
             </li>
             <li>
-                <Link className="link" to="/sync-exim/certificates">Certificates</Link>
+                <Link to="/sync-exim/certificates">Certificates</Link>
             </li>
         </ul>
     </nav>
