48e61183c2675fd2fa490e571e4367b741da3a11
Naveen <naveen80567@gmail.com>
2024-12-09 23:37:48 +0530

    nav changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,53 @@
 import { Link } from "react-router-dom"
 import "./NavBar.css"
+import { useState } from "react"
 
 const NavBar = () => {
+
+    const [right,setRight] = useState('100%');
+
+    const handleClick = () => {
+        if(right === '0%'){
+            setRight('100%');
+        }
+        else{
+            setRight('0%');
+        }
+    }
+
   return (
-    <nav id="primary-nav">
+    <>
+    <nav id="primary-nav" style={{right:right}}>
         <div className="logo">
                 Sync Exim
         </div>
         <ul className="navigation">
-            <li>
+            <li onClick={handleClick}>
                 <Link to="/sync-exim">Home</Link>
             </li>
-            <li>
+            <li onClick={handleClick}>
                 <Link to="/sync-exim/about">About</Link>
             </li>
-            <li>
+            <li onClick={handleClick}>
                 <Link to="/sync-exim/contact">Contact</Link>
             </li>
-            <li>
+            <li onClick={handleClick}>
                 <Link to="/sync-exim/products">Products</Link>
             </li>
-            <li>
+            <li onClick={handleClick}>
                 <Link to="/sync-exim/certificates">Certificates</Link>
             </li>
         </ul>
     </nav>
+    <nav id="secondary-nav">
+        <div className="logo">
+            Sync Exim
+        </div>
+        <div className="menu" onClick={handleClick}>
+            Menu
+        </div>
+    </nav>
+    </>
   )
 }
 
