317a85dd2e061d219ea5787a193406db06a52d8f
Naveen <naveen80567@gmail.com>
2025-01-06 00:23:56 +0530

    ...

--- src/components/Certificates/SinglePageViewer.jsx
+++ src/components/Certificates/SinglePageViewer.jsx
@@ -5,14 +5,16 @@ export default function SinglePage(props) {
   const { pdf } = props;
 
   return (
-    <>
-      <Document file={pdf}>
-        <Page
-          pageNumber={1}
-          renderAnnotationLayer={false}
-          renderTextLayer={false}
-        />
-      </Document>
-    </>
+    <div className="flex items-center flex-col">
+      <div className="flex justify-center p-2 overflow-auto">
+        <Document file={pdf}>
+          <Page
+            pageNumber={1}
+            renderAnnotationLayer={false}
+            renderTextLayer={false}
+          />
+        </Document>
+      </div>
+    </div>
   );
 }
