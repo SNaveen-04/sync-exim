d0beb68f8ede58ea3ba0150db642b1b9126e8346
Naveen <naveen80567@gmail.com>
2025-01-06 22:43:56 +0530

    ...

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -13,31 +13,23 @@ const Certificates = () => {
 
   return (
     <div className="flex flex-col min-h-screen items-center p-4">
-      {/* Buttons to toggle PDFs */}
       <div className="flex gap-4 md:gap-10 mb-6">
         <button
           onClick={() => handleToggle(PDF1)}
-          className="p-2 md:px-4 md:py-2 text-sm bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+          className="p-2 md:px-4 md:py-2 text-xs md:text-sm text-zinc-700 border border-zinc-500 hover:bg-slate-100 rounded-lg"
         >
           IEC Certificate
         </button>
         <button
           onClick={() => handleToggle(PDF2)}
-          className="p-2 md:px-4 md:py-2 text-sm bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+          className="p-2 md:px-4 md:py-2 text-xs md:text-sm text-zinc-700 border border-zinc-500 hover:bg-slate-100 rounded-lg"
         >
           Udyam Registration Certificate
         </button>
       </div>
-
-      {/* Display the selected PDF */}
       {isVisible && (
-        <div className="w-full h-[85vh] border rounded-lg shadow-lg">
-          <iframe
-            src={pdf}
-            className="w-full h-full"
-            title="PDF Viewer"
-            frameBorder="0"
-          />
+        <div className="w-full h-screen border rounded-lg shadow-lg">
+          <iframe src={pdf} className="w-full h-full" title="PDF Viewer" />
         </div>
       )}
     </div>
