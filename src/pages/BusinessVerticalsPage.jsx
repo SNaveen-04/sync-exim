f2b500c0089fe052264e5fd22cb7fcc27834dbd6
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2025-03-01 23:45:02 +0530

    changes fixed in business vertical

--- src/pages/BusinessVerticalsPage.jsx
+++ src/pages/BusinessVerticalsPage.jsx
@@ -42,11 +42,11 @@ const verticals = [
 const BusinessVerticalsPage = () => {
   return (
     <div className="container mx-auto p-4 py-10">
-      <h2 className="uppercase text-[#273296] py-5 text-3xl text-center !font-bold">
+      <h2 className="uppercase text-[#273296] py-5 text-4xl text-center !font-bold">
         Our Business Verticals
       </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {verticals.map((vertical, index) => (
           <div
             key={index}
@@ -56,13 +56,13 @@ const BusinessVerticalsPage = () => {
               <img
                 src={vertical.img}
                 alt={vertical.text}
-                className="w-full h-56 object-cover transform group-hover:scale-105 transition-transform duration-300"
+                className="w-full h-80 object-cover transform group-hover:scale-105 transition-transform duration-300"
               />
               <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:bg-opacity-10 transition-all duration-300"></div>
             </div>
 
-            <div className="p-4">
-              <h3 className="font-semibold text-lg text-[#2c526e] mb-2">
+            <div className="p-5">
+              <h3 className="font-semibold text-xl text-[#2c526e] mb-2 text-center">
                 {vertical.text}
               </h3>
             </div>
